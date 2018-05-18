c27c64b79df5b28a4d79da8ba71ddb7d00dd89bd
Julian Ladisch <julianladisch@users.noreply.github.com>
2018-05-18 08:58:08 +0200

    DIMPT-1: JsonValidator: Add documentation and private constructor, drop unused code

--- src/main/java/org/folio/rest/validate/JsonValidator.java
+++ src/main/java/org/folio/rest/validate/JsonValidator.java
@@ -1,28 +1,26 @@
 package org.folio.rest.validate;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.folio.rest.tools.utils.ObjectMapperTool;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonParseException;
 
 /**
- * @author shale
- *
+ * Utility class for validating JSON.
  */
 public class JsonValidator {
+  private JsonValidator() {
+    throw new UnsupportedOperationException("Cannot instantiate utility class.");
+  }
 
-  public static boolean isValidJsonArray(InputStream entity) throws JsonProcessingException, IOException{
+  /**
+   * @param entity  String to parse as JSON
+   * @return True if entity is a String representation of a JSON array, false otherwise.
+   * @throws JsonParseException  if entity cannot be parsed as JSON
+   * @throws IOException  on missing input
+   */
+  public static boolean isValidJsonArray(String entity) throws IOException {
     return ObjectMapperTool.getMapper().readTree(entity).isArray();
   }
-
-  public static boolean isValidJsonArray(String entity) throws JsonProcessingException, IOException{
-    return ObjectMapperTool.getMapper().readTree(entity).isArray();
-  }
-
-  public static boolean isValidJsonObject(InputStream entity) throws JsonProcessingException, IOException{
-    return ObjectMapperTool.getMapper().readTree(entity).isObject();
-  }
-
 }
