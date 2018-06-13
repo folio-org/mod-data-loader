17500ab862e19794c42d14ca7f7819d0d62be31b
aclseelen <alfons.seelen@hbz-nrw.de>
2018-06-13 17:49:45 +0200

    DIMPT-5: add Mockito, rename test, add before and after

--- src/test/java/org/folio/rest/impl/ProcessorTest.java
+++ src/test/java/org/folio/rest/impl/ProcessorTest.java
@@ -1,12 +1,35 @@
 package org.folio.rest.impl;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ProcessorTest {
 
+  private Processor processor;
+
+  @Before
+  public void setUp() throws Exception {
+
+    Map<String, String> okapiHeaders = new HashMap<>();
+    processor = new Processor("testTenantId", okapiHeaders);
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
   @Test
-  public void process() {
+  public void sourceRecordHandlingTest() {
+
+//    processor.process(false, );
+
   }
 }
